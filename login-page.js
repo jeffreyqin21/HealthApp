560c8823e188f6ef700fdedf5fd841b23bd139e1
Jeffrey Qin <jeffreyqin@Jeffreys-MacBook-Pro.local>
2022-03-29 16:56:20 -0400

    loginpage

---
+++ login-page.js
@@ -0,0 +1,16 @@
+const user = document.getElementById("userInput");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = user.username.value;
+    const password = user.password.value;
+
+    if (username === "user" && password === "434") {
+        alert("You have successfully logged in.");
+        window.location = 'home.html';
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
