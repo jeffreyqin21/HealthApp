bddccab01bdcb0a04c92f1e0a8e48535358c42dc
Jeffrey Qin <jeffreyqin@wireless-10-104-39-151.umd.edu>
2022-03-31 14:44:32 -0400

    small edits

--- diet-page.js
+++ diet-page.js
@@ -5,16 +5,10 @@ const homeClick = document.getElementById("homeClick");
 //eventually want to get calories count from diet page (with export
 const calorieCount = 0;
 const calorieHeader = document.getElementById("calories-header");
-calorieHeader.innerHTML = "Today you have had " + calorieCount + " calories."
 //need to get exercise stat from exercise page as well
 const exerciseDone = false;
 const exerciseHeader = document.getElementById("exercise-header");
 
-if(exerciseDone == false){
-    exerciseHeader.innerHTML = "Good workout today!"
-} else {
-    exerciseHeader.innerHTML = "Remember to workout!"
-}
 
 dietClick.addEventListener("click", (e) => {
      window.location = 'diet.html';   
