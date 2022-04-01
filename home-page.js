ff9ceaa66a30d21090bbd8e04ee7790f3e50dc6e
Jeffrey Qin <jeffreyqin@Jeffreys-MacBook-Pro.local>
2022-04-01 17:34:28 -0400

    added homepage functionality

--- home-page.js
+++ home-page.js
@@ -2,19 +2,74 @@ const dietClick = document.getElementById("dietClick");
 const exerciseClick = document.getElementById("exerciseClick");
 const slumberClick = document.getElementById("slumberClick");
 const homeClick = document.getElementById("homeClick");
-//eventually want to get calories count from diet page (with export
-const calorieCount = 0;
-const calorieHeader = document.getElementById("calories-header");
-calorieHeader.innerHTML = "Today you have had " + calorieCount + " calories."
-//need to get exercise stat from exercise page as well
-const exerciseDone = false;
-const exerciseHeader = document.getElementById("exercise-header");
+const quote = document.getElementById("quote");
+const newQuote = document.getElementById("newQuote");
+const fact = document.getElementById("fact");
+const newFact = document.getElementById("newFact");
 
-if(exerciseDone == false){
-    exerciseHeader.innerHTML = "Good workout today!"
-} else {
-    exerciseHeader.innerHTML = "Remember to workout!"
-}
+
+let a = Math.round(Math.random() * 3 + 1);
+let y = Math.round(Math.random() * 5 + 1)
+
+if(a == 1)
+    quote.innerHTML = "\"It always seems impossible until it's done.\" - Nelson Mandela";
+if(a == 2)
+    quote.innerHTML = "\"If you can dream it, you can do it.\" -Walt Disney";
+if(a == 3)
+    quote.innerHTML = "\"If you fell down yesterday, stand up today.\" -H. G. Wells";
+if(a == 4)
+    quote.innerHTML = "\"Don't watch the clock; do what it does. Keep going.\" -Sam Levenson";
+
+if(y == 1)
+    fact.innerHTML = "Dark chocolate has huge health benefits?";
+if(y == 2)
+    fact.innerHTML = "Broccoli contains more protein than steak?";
+if(y == 3)
+    fact.innerHTML = "Pecans are rich with antioxidants?";
+if(y == 4)
+    fact.innerHTML = "Exercising improves brain performance?";
+if(y == 5)
+    fact.innerHTML = "Exercise prevents signs of ageing?";
+if(y == 6)
+    fact.innerHTML = "Workouts can improve the look of your skin?";
+
+
+newQuote.addEventListener("click", (e) => {
+    let b = Math.round(Math.random() * 3 + 1);
+    while(b == a)
+        b = Math.round(Math.random() * 3 + 1);
+    a = b;
+    if(a == 1)
+        quote.innerHTML = "\"It always seems impossible until it's done.\" - Nelson Mandela";
+    if(a == 2)
+        quote.innerHTML = "\"If you can dream it, you can do it.\" -Walt Disney";
+    if(a == 3)
+        quote.innerHTML = "\"If you fell down yesterday, stand up today.\" -H. G. Wells";
+    if(a == 4)
+        quote.innerHTML = "\"Don't watch the clock; do what it does. Keep going.\" -Sam Levenson";
+    
+})
+
+newFact.addEventListener("click", (e) => {
+    let z = Math.round(Math.random() * 5 + 1);
+    while(z == y)
+        z = Math.round(Math.random() * 5 + 1);
+    y = z;
+    if(y == 1)
+        fact.innerHTML = "Dark chocolate has huge health benefits?";
+    if(y == 2)
+        fact.innerHTML = "Broccoli contains more protein than steak?";
+    if(y == 3)
+        fact.innerHTML = "Pecans are rich with antioxidants?";
+    if(y == 4)
+        fact.innerHTML = "Exercising improves brain performance?";
+    if(y == 5)
+        fact.innerHTML = "Exercise prevents signs of ageing?";
+    if(y == 6)
+        fact.innerHTML = "Workouts can improve the look of your skin?";
+    
+})
+
 
 dietClick.addEventListener("click", (e) => {
      window.location = 'diet.html';   
