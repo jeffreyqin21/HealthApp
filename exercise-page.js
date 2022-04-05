230c0eedf4d051af69dbfe2b8a64b2da85620f8b
Brij Vinaik <brijvinaik@Brijs-MBP.fios-router.home>
2022-04-04 23:42:25 -0400

    more exercise things

---
+++ exercise-page.js
@@ -0,0 +1,38 @@
+window.onload = main
+
+function main () {
+ daily()
+
+
+}
+
+function daily() {
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const d = new Date();
+    let day = days[d.getDay()];
+    document.getElementById("date").innerHTML = day + "'s Focus: ";
+
+    switch (day) {
+        case "Sunday":
+            document.getElementById("focus").innerHTML = "Cardio";
+            break;
+        case "Monday":
+            document.getElementById("focus").innerHTML = "Arms";
+            break;
+        case "Tuesday":
+            document.getElementById("focus").innerHTML = "Legs";
+            break;
+        case "Wednesday":
+            document.getElementById("focus").innerHTML = "Chest";
+            break;
+        case "Thursday":
+            document.getElementById("focus").innerHTML = "Back";
+            break;
+        case "Friday":
+            document.getElementById("focus").innerHTML = "HIIT";
+            break;
+        case "Saturday":
+            document.getElementById("focus").innerHTML = "Rest";
+            break;
+    }
+}
