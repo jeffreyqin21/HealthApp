7b9bc834905a67e861002ad0352d7d26ef5f6e09
Madhur Vinaik <mvinaik@gmail.com>
2022-04-07 14:29:25 -0400

    style

--- exercise-page.js
+++ exercise-page.js
@@ -1,9 +1,41 @@
 window.onload = main
 
 function main () {
- daily()
+    days();
+    document.querySelector("#genBut").onclick = generate;
+    document.querySelector("#submit").onclick = flow;
+
 
 }
+
+function days() {
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const d = new Date();
+    let day = days[d.getDay()];
+    document.getElementById("date").innerHTML = day + "'s Focus: ";
+}
+
+function generate() {
+    document.getElementById("genBut").style.display = "none";
+    document.getElementById("form").style.display = "inline";
+}
+
+function focus() {
+    let l = document.getElementById("focus-choice");
+    let foc = l.options[l.selectedIndex].value;
+    document.getElementById("focus").innerHTML = foc;
+}
+
+function flow () {
+    focus();
+    closeForm();
+}
+
+function closeForm() {
+    document.getElementById("form").style.display = "none";
+
+}
+
 
 
 /* function move() {
@@ -21,35 +53,3 @@ function main () {
   
 }
 */
-
-function daily() {
-    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    const d = new Date();
-    let day = days[d.getDay()];
-    document.getElementById("date").innerHTML = day + "'s Focus: ";
-
-
-    switch (day) {
-        case "Sunday":
-            document.getElementById("focus").innerHTML = "Cardio";
-            break;
-        case "Monday":
-            document.getElementById("focus").innerHTML = "Arms";
-            break;
-        case "Tuesday":
-            document.getElementById("focus").innerHTML = "Legs";
-            break;
-        case "Wednesday":
-            document.getElementById("focus").innerHTML = "Chest";
-            break;
-        case "Thursday":
-            document.getElementById("focus").innerHTML = "Back";
-            break;
-        case "Friday":
-            document.getElementById("focus").innerHTML = "HIIT";
-            break;
-        case "Saturday":
-            document.getElementById("focus").innerHTML = "Rest";
-            break;
-    }
-}
