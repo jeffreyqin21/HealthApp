d90b51959e0b9bc2ee8893b9a4451533af201b07
Madhur Vinaik <mvinaik@gmail.com>
2022-04-13 15:15:33 -0400

    home

--- home-page.js
+++ home-page.js
@@ -1,14 +1,9 @@
-
-const dietClick = document.getElementById("dietClick");
-const exerciseClick = document.getElementById("exerciseClick");
-const slumberClick = document.getElementById("slumberClick");
-const homeClick = document.getElementById("homeClick");
 const quote = document.getElementById("quote");
 const newQuote = document.getElementById("newQuote");
 const fact = document.getElementById("fact");
 const newFact = document.getElementById("newFact");
 const timer = document.getElementById("gameStart");
-const nameTag = document.getElementById("login-header");
+const nameTag = document.getElementById("pump");
 var now = new Date();
 
 if(now.getHours() > 12 && now.getHours() <= 17)
@@ -102,16 +97,3 @@ document.getElementById("gameStart").addEventListener("click", function(){
     console.log(countdown);
 });
 
-
-dietClick.addEventListener("click", (e) => {
-    window.location = 'diet.html';
-})
-exerciseClick.addEventListener("click", (e) => {
-    window.location = 'exercise.html';
-})
-slumberClick.addEventListener("click", (e) => {
-    window.location = 'slumber.html';
-})
-homeClick.addEventListener("click", (e) => {
-    window.location = 'home.html';
-})
